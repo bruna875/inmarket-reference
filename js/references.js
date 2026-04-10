9871bb52fb461d253f178d9602447c99f1805075
bruna875 <bruna@saykudos.co>
2026-04-10 07:57:11 +0200

    Refactor renderRef function for improved layout

--- js/references.js
+++ js/references.js
@@ -11,9 +11,13 @@ function anonName(full) {
 function renderRef(r) {
   var dispName = anonName(r.fullName);
   var initials = r.fullName.split(' ').map(function(w){return w[0];}).join('');
+  var barTop = {deniece_kennedy:32, arthur_haedike:38, teresa_thomas:33, michael_della_penna:33, stanley_turek:30, tonya_may:38, trina_rizzo:38, jason_knapp:38, todd_morris:32};
+  var top = barTop[r.id] !== undefined ? barTop[r.id] : 38;
   var avatarInner = r.photo
     ? '<div style="position:relative;width:100%;height:100%"><img src="'+r.photo+'" alt="'+dispName+'">'
-      + '<div style="position:absolute;top:38%;left:5%;width:90%;height:18%;background:#66C220;opacity:.92;transform:rotate(-2deg);border-radius:2px"></div></div>'
+      + '<div style="position:absolute;top:'+top+'%;left:5%;width:90%;height:18%;background:#66C220;transform:rotate(-2deg);border-radius:2px;display:flex;align-items:center;justify-content:center">'
+      + '<span style="font-size:8px;font-weight:500;color:#fff;letter-spacing:.5px;text-transform:uppercase">Privacy Badge</span>'
+      + '</div></div>'
     : '<div class="ref-avatar-initials">'+initials+'</div>';
   var quoteBlock = r.quote
     ? '<div class="ref-quote-box"><div class="ref-quote-mark">\u201c</div><div class="ref-quote-text">'+r.quote+'</div><div class="ref-attribution">\u2014 '+dispName+'<span>'+r.title+'</span></div></div>'
